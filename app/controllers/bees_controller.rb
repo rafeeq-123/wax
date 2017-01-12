4a5a85480a5a904c68482ef90e201a31cb11f8cd
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-12 09:19:35 -0800

    basic setup in controller

--- app/controllers/bees_controller.rb
+++ app/controllers/bees_controller.rb
@@ -1,2 +1,35 @@
 class BeesController < ApplicationController
+
+  def index
+    @bees = Bee.all
+  end
+
+  def new
+    @bee = Bee.new
+  end
+
+  def create
+    @bee = Bee.new(bees_params)
+    if @bee.save
+      redirect "show"
+    else
+      render "new"
+    end    
+  end
+
+  def show 
+    @bee = Bee.find(params[:id])
+  end
+
+  def destroy
+    @bee = Bee.find(params[:id])
+    @bee.destroy
+    redirect "index"
+  end
+  
+  private
+
+  def bees_params
+    params.require(:bee).permit(:name, :address, :type)
+  end
 end
